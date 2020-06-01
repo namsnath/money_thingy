c0154423523288c318a9e4bee59c0d0e0fe5b310
Namit Nathwani <namsnath@gmail.com>
2020-06-01 17:29:21 +0530

    feat: adds FutureBuilder to renderTiles
    - This allows us to depend on the actual getAccounts() method instead of the property
    - Allows populating initial state

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -3,28 +3,38 @@ import 'package:flutter/widgets.dart';
 import 'package:logging/logging.dart';
 import 'package:money_thingy/database/database.dart';
 import 'package:money_thingy/database/providers/accounts_master_provider.dart';
+import 'package:money_thingy/models/accounts_master.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   final log = Logger('HomePage');
   final dbProvider = DatabaseProvider.dbProvider;
 
-  renderTiles(BuildContext context) {
+  Widget renderTiles(BuildContext context) {
     log.fine('Render Tiles called');
+    return FutureBuilder(
+        future: Provider.of<AccountsMasterProvider>(context).getAccounts(),
+        builder: (BuildContext context,
+            AsyncSnapshot<List<AccountsMaster>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return new LinearProgressIndicator();
+          } else if (snapshot.hasError) {
+            return new Text('Error: ${snapshot.error}');
+          } else {
+            final List<Widget> array = snapshot.data
+                .map((v) => Flexible(
+                    flex: 1,
+                    child: ListTile(
+                      title: Text(v.account),
+                      subtitle: Text(v.id.toString()),
+                    )))
+                .toList();
 
-    final List<Widget> array = Provider.of<AccountsMasterProvider>(context)
-        .accountsList
-        .map((v) => Flexible(
-            flex: 1,
-            child: ListTile(
-              title: Text(v.account),
-              subtitle: Text(v.id.toString()),
-            )))
-        .toList();
-
-    return Row(
-      children: array,
-    );
+            return Row(
+              children: array,
+            );
+          }
+        });
   }
 
   @override
