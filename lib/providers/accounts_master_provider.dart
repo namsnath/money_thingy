93163892be40c3309febbb20d853874af98216f4
Namit Nathwani <namsnath@gmail.com>
2020-06-14 17:31:00 +0530

    feat: makes CRUD operations generic refactor: moves to a provider folder

---
+++ lib/providers/accounts_master_provider.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/widgets.dart';
+import 'package:logging/logging.dart';
+import 'package:money_thingy/database/database.dart';
+import 'package:money_thingy/models/account_master.dart';
+
+class AccountMasterProvider extends ChangeNotifier {
+  final log = Logger('AccountsMasterProvider');
+  final dbProvider = DatabaseProvider.dbProvider;
+
+  List<AccountMaster> _accountsList = [];
+  List<AccountMaster> get accountsList => _accountsList;
+
+  AccountMasterProvider() {
+    // Initialise the state on Provider initialization
+    getAccounts();
+  }
+
+  Future<List<AccountMaster>> getAccounts(
+      {List<String> columns = AccountMaster.columns, String query}) async {
+    final db = await dbProvider.database;
+
+    List<Map<String, dynamic>> result;
+
+    if (query != null) {
+      if (query.isNotEmpty)
+        result = await db.query(AccountMaster.tableName,
+            columns: columns,
+            where: 'description LIKE ?',
+            whereArgs: ["%$query%"]);
+    } else {
+      result = await db.query(AccountMaster.tableName, columns: columns);
+    }
+
+    List<AccountMaster> accounts = result.isNotEmpty
+        ? result.map((item) => AccountMaster.fromDatabaseJson(item)).toList()
+        : [];
+
+    this._accountsList = accounts;
+
+    notifyListeners();
+
+    return accounts;
+  }
+
+  Future<int> addAccount(AccountMaster account) async {
+    final db = await dbProvider.database;
+    var result =
+        await db.insert(AccountMaster.tableName, account.toDatabaseJson());
+
+    this._accountsList = await getAccounts();
+
+    return result;
+  }
+
+  Future<int> updateAccount(AccountMaster account) async {
+    final db = await dbProvider.database;
+
+    var result = await db.update(
+        AccountMaster.tableName, account.toDatabaseJson(),
+        where: '${AccountMaster.colId} = ?', whereArgs: [account.id]);
+
+    this._accountsList = await getAccounts();
+
+    return result;
+  }
+
+  Future<int> deleteAccount(int id) async {
+    final db = await dbProvider.database;
+
+    var result = await db
+        .delete(AccountMaster.tableName, where: 'id = ?', whereArgs: [id]);
+
+    this._accountsList = await getAccounts();
+
+    return result;
+  }
+}
