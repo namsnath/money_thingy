de838f8358b814bd77905356b11fb933bee73998
Namit Nathwani <namsnath@gmail.com>
2020-06-14 17:32:57 +0530

    feat: adds queries to create table and add placeholder data refactor: renames file to be consistent with convention

---
+++ lib/models/account_master.dart
@@ -0,0 +1,39 @@
+class AccountMaster {
+  static const tableName = 'accounts_master';
+
+  static const colId = 'id';
+  static const colAccount = 'account';
+  static const colInstitution = 'institution';
+
+  static const columns = [colId, colAccount, colInstitution];
+
+  static const tableCreateQuery = 'CREATE TABLE $tableName ('
+      '${AccountMaster.colId} INTEGER PRIMARY KEY, '
+      '${AccountMaster.colAccount} TEXT, '
+      '${AccountMaster.colInstitution} TEXT'
+      ')';
+
+  static final initialiseValuesQuery =
+      'INSERT INTO $tableName ($colAccount, $colInstitution) VALUES'
+      '("Personal Expense", "Personal")';
+
+  final int id;
+  final String institution;
+  final String account;
+
+  AccountMaster({this.id, this.account, this.institution});
+
+  factory AccountMaster.fromDatabaseJson(Map<String, dynamic> data) {
+    return AccountMaster(
+      id: data[colId],
+      account: data[colAccount],
+      institution: data[colInstitution],
+    );
+  }
+
+  Map<String, dynamic> toDatabaseJson() => {
+        colId: this.id,
+        colAccount: this.account,
+        colInstitution: this.institution,
+      };
+}
