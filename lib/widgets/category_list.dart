7094828218bed6e265f790f1dd4b844b36e80154
Namit Nathwani <namsnath@gmail.com>
2020-06-27 18:09:28 +0530

    update: CategoryList constructor accepts transaction type filter

--- lib/widgets/category_list.dart
+++ lib/widgets/category_list.dart
@@ -3,18 +3,34 @@ import 'package:flutter_treeview/tree_view.dart';
 import 'package:money_thingy/providers/category_provider.dart';
 import 'package:provider/provider.dart';
 
-class CategoryList extends StatelessWidget {
+class CategoryList extends StatefulWidget {
+  final int transactionTypeIdFilter;
+
+  CategoryList({Key key, @required this.transactionTypeIdFilter})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CategoryListState();
+  }
+}
+
+class _CategoryListState extends State<CategoryList> {
+  String _selectedNode = '';
+
   @override
   Widget build(BuildContext context) {
     return Consumer<CategoryProvider>(builder: (context, category, child) {
-      TreeViewController _treeViewController =
-          TreeViewController(children: category.getCategoryTree(transactionTypeId: 2));
+      TreeViewController _treeViewController = TreeViewController(
+          children: category.getCategoryTree(
+              transactionTypeId: widget.transactionTypeIdFilter));
 
       TreeViewTheme _treeViewTheme = TreeViewTheme(
         expanderTheme: ExpanderThemeData(
-          type: ExpanderType.chevron,
+          type: ExpanderType.plusMinus,
           modifier: ExpanderModifier.none,
           position: ExpanderPosition.start,
+          animated: true,
           // color: Colors.red.shade800,
           size: 20,
         ),
@@ -31,14 +47,50 @@ class CategoryList extends StatelessWidget {
           size: 18,
           color: Colors.grey.shade800,
         ),
-        colorScheme: ColorScheme.light(),
+        colorScheme: ColorScheme.light(background: Colors.transparent),
       );
 
-      return Expanded(
-        child: TreeView(
-          controller: _treeViewController,
-          theme: _treeViewTheme
-        ),
+      return Column(
+        children: <Widget>[
+          Text(
+            'Selected Category',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(_selectedNode),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            height: 400,
+            // constraints: BoxConstraints.expand(),
+            child: TreeView(
+              controller: _treeViewController,
+              theme: _treeViewTheme,
+              allowParentSelect: true,
+              onExpansionChanged: (String key, bool expanded) {
+                Node node = _treeViewController.getNode(key);
+                if (node != null) {
+                  var updated = _treeViewController.updateNode(
+                      key, node.copyWith(expanded: expanded));
+                  setState(() {
+                    _treeViewController =
+                        _treeViewController.copyWith(children: updated);
+                  });
+                }
+              },
+              onNodeTap: (key) {
+                setState(() {
+                  _treeViewController =
+                      _treeViewController.copyWith(selectedKey: key);
+                  _selectedNode = category.categoryHierarchy.firstWhere(
+                          (element) => element['id'].toString() == key)[
+                      'categoryDisplayName'];
+                });
+              },
+              onNodeDoubleTap: (key) => print('Double Tapped: $key'),
+            ),
+          ),
+        ],
       );
     });
   }
