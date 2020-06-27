65e27c1663090c73598fb3f07e545c415482b42e
Namit Nathwani <namsnath@gmail.com>
2020-06-27 18:06:33 +0530

    feat: adds transaction provider

--- lib/providers/transaction_provider.dart
+++ lib/providers/transaction_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:logging/logging.dart';
 import 'package:money_thingy/database/database.dart';
 import 'package:money_thingy/models/transaction.dart';
+import 'package:money_thingy/utils/datetime_util.dart';
 
 class TransactionProvider extends ChangeNotifier {
   final log = Logger('TransactionProvider');
@@ -10,9 +11,63 @@ class TransactionProvider extends ChangeNotifier {
   List<Transaction> _transactionList = [];
   List<Transaction> get transactionList => _transactionList;
 
+  String getAggregateQuery(int startTime, int endTime) => """
+    SELECT
+      COALESCE(SUM(${Transaction.colDebitAmount}), 0.0) as debitAggregate
+      , COALESCE(SUM(${Transaction.colCreditAmount}), 0.0) as creditAggregate
+    FROM ${Transaction.tableName}
+    WHERE
+      ${Transaction.colTransactionTime} >= $startTime
+      AND ${Transaction.colTransactionTime} <= $endTime
+  """;
+
   TransactionProvider() {
     // Initialise the state on Provider initialization
     getTransactions();
+    getAggregates();
+  }
+
+  Future<Map<String, double>> getAggregates({DateTime date}) async {
+    final db = await dbProvider.database;
+
+    if (date == null) {
+      date = DateTime.now();
+    }
+    Map<String, double> aggregates = {
+      'day': 0.0,
+      'week': 0.0,
+      'month': 0.0,
+      'year': 0.0,
+    };
+
+    List<Map<String, dynamic>> dayAggregate =
+        await db.rawQuery(getAggregateQuery(
+      DateTimeUtil.startOfDay(date).millisecondsSinceEpoch,
+      DateTimeUtil.endOfDay(date).millisecondsSinceEpoch,
+    ));
+
+    List<Map<String, dynamic>> weekAggregate =
+        await db.rawQuery(getAggregateQuery(
+      DateTimeUtil.startOfWeek(date).millisecondsSinceEpoch,
+      DateTimeUtil.endOfWeek(date).millisecondsSinceEpoch,
+    ));
+
+    List<Map<String, dynamic>> monthAggregate =
+        await db.rawQuery(getAggregateQuery(
+      DateTimeUtil.startOfMonth(date).millisecondsSinceEpoch,
+      DateTimeUtil.endOfMonth(date).millisecondsSinceEpoch,
+    ));
+
+    List<Map<String, dynamic>> yearAggregate =
+        await db.rawQuery(getAggregateQuery(
+      DateTimeUtil.startOfYear(date).millisecondsSinceEpoch,
+      DateTimeUtil.endOfYear(date).millisecondsSinceEpoch,
+    ));
+
+    // log.info(dayAggregate);
+    // log.info(weekAggregate);
+    // log.info(monthAggregate);
+    // log.info(yearAggregate);
   }
 
   Future<List<Transaction>> getTransactions(
