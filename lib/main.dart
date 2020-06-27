378e241288179007252836e60af99357380cca9b
Namit Nathwani <namsnath@gmail.com>
2020-06-27 18:09:42 +0530

    update: adds Providers to top-level main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:money_thingy/providers/accounts_master_provider.dart';
+import 'package:money_thingy/providers/category_provider.dart';
+import 'package:money_thingy/providers/forms/transaction_form_validation_provider.dart';
+import 'package:money_thingy/providers/transaction_provider.dart';
+import 'package:money_thingy/providers/transaction_type_provider.dart';
+import 'package:money_thingy/screens/transaction_update.dart';
 import 'package:money_thingy/service_locator.dart';
 import 'package:provider/provider.dart';
 
@@ -9,14 +14,21 @@ import 'package:money_thingy/screens/home_page.dart';
 
 void main() {
   setupLocator();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AccountMasterProvider())
-      ],
-      child: MyApp(),
-    )
-  );
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+          create: (_) => AccountMasterProvider(), lazy: false),
+      ChangeNotifierProvider(
+        create: (_) => TransactionTypeProvider(),
+        lazy: false,
+      ),
+      ChangeNotifierProvider(create: (_) => TransactionProvider()),
+      ChangeNotifierProvider(create: (_) => CategoryProvider()),
+      ChangeNotifierProvider(
+          create: (_) => TransactionFormValidationProvider()),
+    ],
+    child: MyApp(),
+  ));
   DatabaseProvider.dbProvider.init();
 
   Logger.root.level = Level.FINE; // defaults to Level.INFO
@@ -30,15 +42,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Money Thingy',
+      // theme: ThemeData(
+      //   brightness: Brightness.light,
+      //   primarySwatch: Colors.blue,
+      //   visualDensity: VisualDensity.adaptivePlatformDensity,
+      // ),
+      // darkTheme: ThemeData(
+      //   brightness: Brightness.dark,
+      // ),
+      // home: HomePage(),
       theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: HomePage(),
+      home: TransactionUpdate(),
     );
   }
 }
