2b836bdd55792a5d81940c1f0e7b730a7132f921
Namit Nathwani <namsnath@gmail.com>
2020-06-27 18:05:55 +0530

    feat: adds basic account summary view

---
+++ lib/widgets/account_summary.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:money_thingy/providers/accounts_master_provider.dart';
+import 'package:provider/provider.dart';
+
+class AccountSummary extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AccountMasterProvider>(
+        builder: (context, accountsMaster, child) {
+      final List<Widget> array = accountsMaster.accountsList
+          .map((v) => Flexible(
+              flex: 1,
+              child: ListTile(
+                title: Text(v.account),
+                subtitle: Text(v.id.toString()),
+              )))
+          .toList();
+
+      return Row(children: array);
+    });
+  }
+}
